f029a21be3284a59862a7e0795823b81453b87a3
ogty <mofuiti1803@gmail.com>
2022-01-01 23:33:29 +0900

    add

---
+++ component/code_acquisition.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class CodeAcquisition:
+    def __init__(self, cateogry_number) -> None:
+        self.cateogry_number = cateogry_number
+        self.codes = []
+
+    def max_page_scraping(self) -> int:
+        url = f"https://info.finance.yahoo.co.jp/ranking/?kd={self.cateogry_number}&tm=d&vl=a&mk=1&p=1"
+        html = requests.get(url)
+        soup = BeautifulSoup(html.content, "html.parser")
+        tag = soup.select("[class='ymuiPagingBottom clearFix']")
+        result = int(tag[0].text.split("...")[1].rstrip("次へ"))
+        return result
+
+    def code_scraping(self):
+        for i in self.max_page_scraping():
+            url = f"https://info.finance.yahoo.co.jp/ranking/?kd={self.cateogry_number}&tm=d&vl=a&mk=1&p={i}"
+            html = requests.get(url)
+            soup = BeautifulSoup(html.content, "html.parser")
+            data = soup.select("[class='txtcenter']")
+            self.codes += [data[i].text for i in range(len(data)) if not i % 2 == 0]
