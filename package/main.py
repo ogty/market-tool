232050ede71b1c0982a95d120030cd7360eecdf4
ogty <mofuiti1803@gmail.com>
2022-01-04 20:04:35 +0900

    update

--- package/main.py
+++ package/main.py
@@ -6,12 +6,15 @@ import os
 import pandas as pd
 import requests
 import schedule
+import sys
 import time
 import tweepy
 import urllib
 
-load_dotenv()
 
+load_dotenv()
+oldest_month = 0
+ALL_COMPANIES = 0
 
 client = tweepy.Client(
     os.environ["BEARER_TOKEN"], 
@@ -21,12 +24,13 @@ client = tweepy.Client(
     os.environ["ACCESS_TOKEN_SECRET"]
 )
 
-ALL_COMPANIES = 0
+def loading_spinner():
+    while True:
+        for cursor in "|/-\\":
+            yield cursor
 
 # download latest data file and update constant variable
-def download_update() -> None:
-    global ALL_COMPANIES
-
+def download_update() -> int:
     url = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
     save_path = "./data/data_j.xls"
     urllib.request.urlretrieve(url, save_path)
@@ -36,10 +40,20 @@ def download_update() -> None:
 
     df = pd.read_csv("./data/data_j.csv")
     codes = [i for i in df["コード"].to_list() if len(str(i)) == 4]
+    result = len(codes)
 
-    ALL_COMPANIES = len(codes)
+    return result
 
 def trend() -> None:
+    global ALL_COMPANIES
+    global oldest_month 
+
+    latest_month = datetime.datetime.now().month
+
+    if latest_month != oldest_month:
+        ALL_COMPANIES = download_update()
+        oldest_month = latest_month
+
     url = "https://info.finance.yahoo.co.jp/ranking/?kd=1&tm=d&mk=1"
     html = requests.get(url)
     soup = BeautifulSoup(html.content, "html.parser")
@@ -47,14 +61,12 @@ def trend() -> None:
     result = data[0].text
     up_companies = result.split("/")[1].replace("件中", "")
     
-    up_rate = round(int(up_companies) / ALL_COMPANIES, 3)
-    down_rate = round(1.0 - up_rate, 3)
+    up_rate = round((int(up_companies) / ALL_COMPANIES) * 100, 3)
+    down_rate = round((1.0 - up_rate) * 100, 3)
 
-    now = datetime.datetime.now()
-    twitter_message = f"{now}\nUP：{up_rate * 100}%\nDOWN：{down_rate * 100}%"
-    slack_message = f"{now.strftime('%H:%M')} | UP：{up_rate * 100}% | DOWN：{down_rate * 100}%"
-
-    print(f"{now} - UP：{up_rate * 100}% - DOWN：{down_rate * 100}%")
+    now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
+    message = f"{now} | UP: {up_rate} | DOWN: {down_rate}"
+    twitter_message = f"{now}\nUP：{up_rate}\nDOWN：{down_rate}"
 
     # Twitter bot
     client.create_tweet(text=twitter_message)
@@ -70,11 +82,11 @@ def trend() -> None:
         )
     )
 
-def generate_schedule(hour_list, waste_schedule=[]) -> list:
+def generate_schedule(hour_list, step=30, include=True, waste_schedule=[]) -> list:
     time_schedule = []
 
     for hour in hour_list:
-        for minute in range(0, 51, 10):
+        for minute in range(0, 51, step):
             if len(str(hour)) == 1:
                 hour = f"0{hour}"
             if len(str(minute)) == 1:
@@ -82,13 +94,19 @@ def generate_schedule(hour_list, waste_schedule=[]) -> list:
 
             time_schedule.append(f"{hour}:{minute}")
 
+    if include:
+        if hour_list[-1] == 23:
+            pass
+        else:
+            time_schedule.append(f"{hour_list[-1] + 1}:00")
+        
     for waste in waste_schedule:
         time_schedule.remove(waste)
 
     return time_schedule
 
 # Get an annual leave schedule
-def market_holidays(path: str) -> None:
+def market_holidays(year: str, path: str) -> None:
     url = "https://www.jpx.co.jp/corporate/about-jpx/calendar/index.html"
     html = requests.get(url)
     soup = BeautifulSoup(html.content, "html.parser")
@@ -108,7 +126,7 @@ def is_open() -> bool:
     
     path = f"./data/{year}.txt"
     if not os.path.exists(path):
-        market_holidays(path)
+        market_holidays(year, path)
     
     with open(path, "r", encoding="utf-8") as f:
         holidays = [holiday.rstrip() for holiday in f]
@@ -126,19 +144,14 @@ def is_open() -> bool:
 
 # Create schedule
 waste_schedule = ["11:40", "11:50", "12:00", "12:10", "12:20"]
-time_schedule = generate_schedule(range(9, 15), waste_schedule)
-time_schedule.append("15:00")
+time_schedule = generate_schedule(range(9, 15), step=10, waste_schedule=waste_schedule)
 
-oldest_month = 0
+loading = loading_spinner()
 while True:
-    latest_month = datetime.datetime.now().month
-
-    # Initialize and Update "./data/data_j.csv"
-    if latest_month != oldest_month:
-        download_update()
-        oldest_month = latest_month
-
     if is_open():
+        today = datetime.datetime.now().strftime("%Y/%m/%d")
+        print(f"\n\n{today}")
+
         [schedule.every().day.at(i).do(trend) for i in time_schedule]
 
         while True:
@@ -148,4 +161,12 @@ while True:
             schedule.run_pending()
             time.sleep(1)
     else:
-        time.sleep(1)
+        print("Holiday  ", end="\b")
+        while True:
+            if is_open():
+                break
+
+            sys.stdout.write(next(loading))
+            sys.stdout.flush()
+            time.sleep(0.5)
+            sys.stdout.write("\b")
