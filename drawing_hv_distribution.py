126373cc185a0bd62c1ed1d76fe67fa5e6c6da8e
ogty <mofuiti1803@gmail.com>
2022-01-04 19:40:27 +0900

    fix: error

--- drawing_hv_distribution.py
+++ drawing_hv_distribution.py
@@ -8,8 +8,8 @@ with open("./data/hv.txt", "r", encoding="utf-8") as f:
 
 data = [i for i in data if not i > 100]
 
-median = statistics.median(data2)
-mean = statistics.mean(data2)
+median = statistics.median(data)
+mean = statistics.mean(data)
 
 fig = plt.figure()
 gs = gridspec.GridSpec(2, 1, height_ratios=[1, 5])
@@ -18,7 +18,7 @@ gs = gridspec.GridSpec(2, 1, height_ratios=[1, 5])
 ax = plt.subplot(gs[0])
 ax.set_xticks([i for i in range(0, 101, 10)])
 ax.set_title("Historical Volatility(HV) Distribution")
-ax.boxplot(data2, vert=False, sym="-")
+ax.boxplot(data, vert=False, sym="-")
 ax.axes.yaxis.set_visible(False)
 
 # Histgram
@@ -27,7 +27,7 @@ ax.set_ylabel("Frequency")
 ax.set_xlabel("HV(%)")
 ax.set_yticks([i for i in range(0, 251, 25)])
 ax.set_xticks([])
-ax.hist(data2, bins=80, histtype="step")
+ax.hist(data, bins=80, histtype="step")
 ax.axvline(median, color="k", linestyle="dashed", linewidth=1, label="median")
 ax.axvline(mean, color="k", linestyle="dotted", linewidth=1, label="mean")
 ax.tick_params(length=0)
