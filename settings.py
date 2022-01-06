58817113583929c9e0078b40a659e72374007b66
ogty <mofuiti1803@gmail.com>
2022-01-07 07:17:29 +0900

    update

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 TOP_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(TOP_DIR, "data")
-LOGS_DIR = os.path.join(TOP_DIR, "logs")
+LOGS_DIR = os.path.join(DATA_DIR, "logs")
 IMAGES_DIR = os.path.join(TOP_DIR, "images")
 MARKET_DATA_DIR = os.path.join(DATA_DIR, "market_data")
 TOTALLING_DATA_DIR = os.path.join(DATA_DIR, "totalling_data")
