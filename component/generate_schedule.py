b4be53d0b35c5d4effb78489a48e4a296dc8e629
ogty <mofuiti1803@gmail.com>
2022-01-07 00:59:28 +0900

    fix: range

--- component/generate_schedule.py
+++ component/generate_schedule.py
@@ -1,8 +1,8 @@
-def generate_schedule(hour_list: list, step=30, include=True, waste_schedule=[]) -> list:
+def generate_schedule(hour_list: list, step=30, include=True, addition=[], delete=[]) -> list:
     time_schedule = []
 
     for hour in hour_list:
-        for minute in range(0, 51, step):
+        for minute in range(0, 60, step):
             if len(str(hour)) == 1:
                 hour = f"0{hour}"
             if len(str(minute)) == 1:
@@ -15,8 +15,15 @@ def generate_schedule(hour_list: list, step=30, include=True, waste_schedule=[])
             pass
         else:
             time_schedule.append(f"{hour_list[-1] + 1}:00")
-        
-    for waste in waste_schedule:
-        time_schedule.remove(waste)
+    
+    for add_schedule in addition:
+        time_schedule.append(add_schedule)
+
+    for del_schedule in delete:
+        time_schedule.remove(del_schedule)
 
     return time_schedule
+
+if __name__ == "__main__":
+    result = generate_schedule(range(9, 15), step=30, addition=["09:05"])
+    print(result)
