a732ed00f7292223349a446cef418a68c5c94c67
ogty <mofuiti1803@gmail.com>
2022-01-07 08:35:56 +0900

    update

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ latest_day = 0
 loading = loading.loading_spinner()
 while True:
     if mt.is_open():
-        today = datetime.datetime.now().strftime("%Y/%m/%d")
-        print(f"\n\n{today}")
+        print(f"\n\n{datetime.datetime.now().strftime('%Y/%m/%d')}")
 
         [schedule.every().day.at(i).do(mt.trend) for i in time_schedule]
         schedule.every().day.at("15:05").do(totalling.totalling)
